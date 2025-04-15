7bb83479faa9d92679d3a85c9e189dcc7e58124f
Nolann L <lescopnolann@gmail.com>
2025-04-15 10:54:44 +0200

    Update

--- Livrable.sql
+++ Livrable.sql
@@ -1,5 +1,8 @@
 /**
 Requêtes SQL - SAE S2.04
+Prénom : Nolann / Matthieu / Arthur / Marin
+Nom : LESCOP / GOUELO / JAN / WEIS
+Groupe : 1D1
 
 ---------------------------------------------------
 /* SCHEMA RELATIONNEL
@@ -8,85 +11,159 @@ Compteur(numero (1), libelle, leQuartier = @Quartier.id)
 Quartier(id (1), nomQuartier, longueurPiste)
 Date(date (1), jour (NN), tempMoy, vacances)
 Comptage([numCompteur = @Compteur.numero, laDate = @Date.date](1), nbVelo, probaAnomalie)
+
 */
 ---------------------------------------------------
 */
 
--- Question 1 : Quels sont les compteurs (numéro et libellé) situés dans le quartier "Centre-ville" ?
+
+-- Requête 1 : Quels sont les compteurs (numéro et libellé) situés dans le quartier "Centre-ville" ? (jointure interne)
 SELECT DISTINCT numero, libelle
 FROM Compteur
     JOIN Quartier ON leQuartier = id
 WHERE nomQuartier = 'Centre Ville';
+/* Résultat de la requête : 22 tuples
+# numero, libelle
+'664', 'Bonduelle vers sud'
+'665', 'Bonduelle vers Nord'
+'666', 'Pont Audibert vers Sud'
+'674', 'Pont Haudaudine vers Sud'
+'675', 'Pont Haudaudine vers Nord'
+...
+*/
 
 
--- Question 2 : Quels sont les quartiers qui ont la même longueur de piste cyclable ?
+-- Requête 2 : Quels sont les quartiers qui ont la même longueur de piste cyclable ? (auto-jointure)
 SELECT Q1.nomQuartier
 FROM Quartier Q1, Quartier Q2
 WHERE Q1.id != Q2.id
 AND Q1.longueurPiste = Q2.longueurPiste;
+/* Résultat de la requête : 0 tuple
+
+*/
 
 
--- Question 3 : Afficher tous les quartiers avec leurs compteurs, même ceux qui n'ont pas de compteurs.
+-- Requête 3 : Afficher tous les quartiers avec leurs compteurs, même ceux qui n'ont pas de compteurs. (jointure externe)
 SELECT numero, libelle, nomQuartier
 FROM Compteur
     LEFT JOIN Quartier ON leQuartier = id;
+/* Résultat de la requête : 76 tuples
+ # numero, libelle, nomQuartier
+'89', 'Coteaux vers Ouest', NULL
+'664', 'Bonduelle vers sud', 'Centre Ville'
+'665', 'Bonduelle vers Nord', 'Centre Ville'
+'666', 'Pont Audibert vers Sud', 'Centre Ville'
+'667', 'EntrÃ©e pont Audibert vers Nord', 'Ile de Nantes'
+...
+*/
 
 
--- Question 4 : Lister toutes les dates avec les comptages, y compris les dates où aucun comptage n'a été effectué.
+-- Requête 4 : Lister toutes les dates avec les comptages, y compris les dates où aucun comptage n'a été effectué. (jointure externe)
 SELECT date, laDate
 FROM Date
     LEFT JOIN Comptage ON laDate = date;
-    
+/* Résultat de la requête : 1000 tuples
+ # date, laDate
+'2020-01-01', '2020-01-01'
+'2020-01-01', '2020-01-01'
+'2020-01-01', '2020-01-01'
+'2020-01-01', '2020-01-01'
+'2020-01-01', '2020-01-01'
+...
+*/
 
 
--- Question 5 : Quels compteurs n'ont jamais enregistré de comptage avec une anomalie (probaAnomalie > 0) ?
+-- Requête 5 : Quels compteurs n'ont jamais enregistré de comptage avec une anomalie FORTE ? (sous-requête avec NOT IN)
 SELECT DISTINCT numCompteur
 FROM Comptage
 WHERE numCompteur NOT IN (SELECT DISTINCT numCompteur
                            FROM Comptage
                            WHERE UPPER(probaAnomalie) = 'FORTE'
                           );
+/* Résultat de la requête : 2 tuples
+# numCompteur
+'725'
+'981'
+*/
 
 
--- Question 6 : Quels quartiers ont des compteurs qui ont enregistré plus de 100 vélos un jour de vacances ?
-SELECT DISTINCT numCompteur
-FROM Comptage, Date
-AND nbVelo > 100
-AND numCompteur IN (SELECT numCompteur
-                     FROM Comptage, Date
-                     WHERE laDate = date
-                     AND UPPER(vacances) != 'HORS VACANCES'
-                    )
+-- Requête 6 : Quels quartiers ont des compteurs qui ont enregistré plus de 100 vélos un jour de vacances ? (sous-requête avec IN)
+SELECT DISTINCT nomQuartier
+FROM Quartier
+    JOIN Compteur ON leQuartier = id
+WHERE numero IN ( SELECT DISTINCT numCompteur
+                  FROM Comptage
+                    JOIN Date ON laDate = date
+                  WHERE nbVelo > 100
+                  AND UPPER(vacances) != 'HORS VACANCES'
+                )
 ;
+/* Résultat de la requête : 9 tuples
+# nomQuartier
+'Centre Ville'
+'Dervallières - Zola'
+'Hauts Pavés - Saint Félix'
+'Malakoff - Saint-Donatien'
+'Ile de Nantes'
+'Nantes Nord'
+'Nantes Sud'
+'Pont Rousseau'
+'Ragon'
+*/
 
 
--- Question 7 : Quelles dates ont au moins un comptage avec une probabilité d'anomalie ?
+-- Requête 7 : Quelles dates ont au moins un comptage avec une probabilité d'anomalie FORTE ? (sous-requête avec EXISTS)
 SELECT DISTINCT date
 FROM Date
 WHERE EXISTS (
     SELECT numCompteur
-    FROM Commpteur
+    FROM Comptage
     WHERE laDate = date
-    AND UPPER(probaAnomalie) = 'FORTE'
+        AND UPPER(probaAnomalie) = 'FORTE'
 );
+/* Résultat de la requête : 728 tuples
+# date
+'2020-01-01'
+'2020-01-02'
+'2020-01-03'
+'2020-01-04'
+'2020-01-05'
+'2020-01-06'
+...
+*/
 
 
--- Question 8 : Quels quartiers n'ont aucun compteur ayant enregistré de données ?
-SELECT leQuartier
-FROM Compteur
+-- Requête 8 : Quels quartiers n'ont aucun compteur ayant enregistré de données ? (sous-requête avec NOT EXISTS)
+SELECT nomQuartier
+FROM Quartier
+    JOIN Compteur ON leQuartier = id
 WHERE NOT EXISTS (
     SELECT *
     FROM Comptage
     WHERE numCompteur = numero
 );
+/* Résultat de la requête : 2 tuples
+# nomQuartier
+'Doulon - Bottière'
+'Doulon - Bottière'
+*/
 
--- Question 9 : Quel est le nombre total de vélos comptés sur l'ensemble des comptages ?
-SELECT COUNT(*) AS total_lignes
-FROM ComptageVelo;
 
--- Question 10 : Quelle est la température moyenne la plus élevée enregistrée parmi toutes les dates ?
-SELECT AVG(nombre_velos) AS moyenne_globale
-FROM ComptageVelo;
+-- Requête 9 : Quel est le nombre total de vélos comptés sur l'ensemble des comptages ? (Fonction de groupe sans regroupement)
+SELECT SUM(nbVelo) AS totalVelos
+FROM Comptage;
+/* Résultat de la requête : 1 tuple
+# totalVelos
+'39448940'
+*/
+
+-- Requête 10 : Quelle est la température moyenne la plus élevée enregistrée parmi toutes les dates ? (Fonction de groupe sans regroupement)
+SELECT MAX(tempMoy) AS tempMax
+FROM Date;
+/* Résultat de la requête : 1 tuple
+# tempMax
+'30.95'
+*/
 
 -- Question 11 : Quel est le nombre moyen de vélos comptés par quartier ?
 SELECT jour_semaine, AVG(nombre_velos) AS moyenne
